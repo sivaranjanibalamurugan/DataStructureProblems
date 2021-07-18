f1402cabf67bf197b071ac59ca7bde1c15e5f820
unknown <sivaranjanib99@gmail.com>
2021-07-18 16:12:03 +0530

    [siva]NUMOFBST finding the number of nodes in a binary search tree

---
+++ NumberofBinarySearchTree/NumberofBST.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberofBinarySearchTree
+{
+    class NumberofBST
+    {
+        //T(0)=1
+        //T(1)=1
+        //T(2)=T(0)*T(1)+T(1)*T(0)
+        public static int NumberOfTrees(int n)
+        {
+            if (n <= 1) return 1;
+
+            int sum = 0;
+            int left = 0, right = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                left = NumberOfTrees(i - 1);
+                right = NumberOfTrees(n - i);
+                sum += (left * right);
+            }
+            return sum;
+        }
+    }
+}
---
+++ NumberofBinarySearchTree/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumberofBinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to binary search tree!!!");
+            Console.WriteLine("Number of Binary Search tree with n node");
+            int trees = NumberofBST.NumberOfTrees(3);
+            Console.WriteLine("Total number of tress ={0}", trees);
+        }
+    }
+}
